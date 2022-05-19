a66d0cfa6d5d25de331b485907e14fc6c7d34431
askok18 <105781703+askok18@users.noreply.github.com>
2022-05-19 14:20:15 +0530

    Add files via upload

---
+++ quize/quiz.js
@@ -0,0 +1,106 @@
+//Question bank
+var questionBank= [
+    {
+        question : 'what is the t-shirt no.of virat kohli in ODI and T20Is',
+        option : ['8','11','10','18'],
+        answer : '18'
+    },
+    {
+        question : 'In which year did kohli made his T20I debut',
+        option : ['2007','2010','2009','2008'],
+        answer : '2010'
+    },
+    {
+        question : 'Agaist which team did virat kohli scored his firt test hunderd',
+        option : ['west Indies','Newzealand','Austraila','paksitan'],
+        answer : 'Austraila'
+    },
+    {
+        question : 'which test bowler has dismissed virat kohli the most times till may 2019',
+        option : ['Nathon lyon','james Anderson','moin ali','mitchell strac'],
+        answer : 'james Anderson'
+    },
+    {
+        question : 'which team as scored highest traget in IPL',
+        option : ['srh','rcb','csk','mi'],
+        answer : 'rcb'
+    }
+]
+
+var question= document.getElementById('question');
+var quizContainer= document.getElementById('quiz-container');
+var scorecard= document.getElementById('scorecard');
+var option0= document.getElementById('option0');
+var option1= document.getElementById('option1');
+var option2= document.getElementById('option2');
+var option3= document.getElementById('option3');
+var next= document.querySelector('.next');
+var points= document.getElementById('score');
+var span= document.querySelectorAll('span');
+var i=0;
+var score= 0;
+
+//function to display questions
+function displayQuestion(){
+    for(var a=0;a<span.length;a++){
+        span[a].style.background='none';
+    }
+    question.innerHTML= 'Q.'+(i+1)+' '+questionBank[i].question;
+    option0.innerHTML= questionBank[i].option[0];
+    option1.innerHTML= questionBank[i].option[1];
+    option2.innerHTML= questionBank[i].option[2];
+    option3.innerHTML= questionBank[i].option[3];
+    stat.innerHTML= "Question"+' '+(i+1)+' '+'of'+' '+questionBank.length;
+}
+
+//function to calculate scores
+function calcScore(e){
+    if(e.innerHTML===questionBank[i].answer && score<questionBank.length)
+    {
+        score= score+1;
+        document.getElementById(e.id).style.background= 'limegreen';
+    }
+    else{
+        document.getElementById(e.id).style.background= 'tomato';
+    }
+    setTimeout(nextQuestion,300);
+}
+
+//function to display next question
+function nextQuestion(){
+    if(i<questionBank.length-1)
+    {
+        i=i+1;
+        displayQuestion();
+    }
+    else{
+        points.innerHTML= score+ '/'+ questionBank.length;
+        quizContainer.style.display= 'none';
+        scoreboard.style.display= 'block'
+    }
+}
+
+//click events to next button
+next.addEventListener('click',nextQuestion);
+
+//Back to Quiz button event
+function backToQuiz(){
+    location.reload();
+}
+
+//function to check Answers
+function checkAnswer(){
+    var answerBank= document.getElementById('answerBank');
+    var answers= document.getElementById('answers');
+    answerBank.style.display= 'block';
+    scoreboard.style.display= 'none';
+    for(var a=0;a<questionBank.length;a++)
+    {
+        var list= document.createElement('li');
+        list.innerHTML= questionBank[a].answer;
+        answers.appendChild(list);
+    }
+}
+
+
+displayQuestion();
